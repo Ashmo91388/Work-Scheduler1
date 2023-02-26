48a31d84a020d0b1fb1df21e95756d6b64d26e07
cameronmcnz~clear <ashly_mossette@yahoo.com>
2023-02-26 15:14:42 -0800

    modifed js, code still broken

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ var todaysDate = dayjs();
 $('#currentDay').text(dayjs().format('dddd, MMM D, YYYY'));
 
 
-$(document).ready(function () {
-
-  // var presentHour = dayjs().hour();
+$(document).ready(function (){
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -22,37 +20,30 @@ $(document).ready(function () {
     localStorage.setItem(time, text); //save info to the local storage 
   })
 
-  // $(".saveBtn").click(function (event) {
-  //   event.preventDefault();
-  //   var value = $(this).siblings(".color-time-block").val();
-  //   var time = $(this).parent().attr("id").split("-")[1];
-  //   localStorage.setItem(time, value);
-  // });
-
   //  How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  function trackCurrentTime() {
+
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+
+
+
+  function trackCurrentTime(){
     var timeNow = dayjs().hour();
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-
-
-    
-
-    $(".color-time-block").each(function () {
+    $(".color-time-block").each(function(){
       var blockTime = parseInt($(this).attr("id").split("hour")[1]); //jquery loop to go through times 
 
       // conditional statements to check future, past, present times
       if (blockTime < timeNow) {
         $(this).removeClass("future");
         $(this).removeClass("present");
-        $(this).addClass("past");        
-        
+        $(this).addClass("past");
+
       }
       else if (blockTime === timeNow) {
         $(this).removeClass("past");
@@ -84,4 +75,4 @@ $(document).ready(function () {
   // TODO: Add code to display the current date in the header of the page.
   trackCurrentTime();
 })
-// });
+
